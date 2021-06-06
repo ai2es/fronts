844a21de6bdf04c13438b006c547fcca2b64b8e9
andrewjustin <76486321+andrewjustin@users.noreply.github.com>
2021-06-06 14:38:45 -0500

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,11 +2,13 @@
 Function that creates, trains, and validates a Unet model.
 
 Code written by: Andrew Justin (andrewjustin@ou.edu)
-Last updated: 6/1/2021 3:38 PM CDT
+Last updated: 6/6/2021 11:45 AM CDT
 """
 
 import random
 import argparse
+
+import keras.utils
 from keras_unet_collection import models, losses
 import tensorflow as tf
 from keras.optimizers import Adam
@@ -14,120 +16,154 @@ import pickle
 import numpy as np
 from tensorflow.keras.utils import to_categorical
 import file_manager as fm
+import os
+import evaluate_model
 
-def training_generator(frontobject_conus_files, surfacedata_conus_files, map_size, input_name, output_name, batch_size):
-    """
-    Training generator for the Unet that generates random batches of data.
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, front_files_list, sfcdata_files_list, map_size, batch_size):
+        self.front_files_list = front_files_list
+        self.sfcdata_files_list = sfcdata_files_list
+        self.map_size = map_size
+        self.batch_size = batch_size
 
-    Parameters
-    ----------
-    frontobject_conus_files: list
-        List of all files containing fronts. Includes files with no fronts present in their respective domains.
-    surfacedata_conus_files: list
-        List of all files containing surface data.
-    input_name: str
-        Name of the input layer of the Unet.
-    output_name: str
-        Name of the output layer of the Unet.
-    map_size: int
-        Integer that determines the size of the window to be fed into the Unet. (e.g. Setting this to 32 would create a
-        Unet that takes input shape (batch_size, 32, 32, num_predictors)). This MUST be a multiple of 2^n.
-    batch_size: int
-        Number of images/datasets to process for each step of each epoch in the training generator. This is determined
-        by the 'train_batch_size' argument.
-    """
-    while True:
-        front_dss = np.empty(shape=(batch_size,map_size,map_size,3))
-        sfcdata_dss = np.empty(shape=(batch_size,map_size,map_size,6))
-        for i in range(batch_size):
-            identifiers = 0
+    def __len__(self):
+        return int(np.floor(len(self.front_files_list) / self.batch_size))
+
+    def __getitem__(self, index):
+        input, output = self.__data_generation()
+        return input, output
+
+    def __data_generation(self):
+        front_dss = np.empty(shape=(self.batch_size,self.map_size,self.map_size,3))
+        sfcdata_dss = np.empty(shape=(self.batch_size,self.map_size,self.map_size,6))
+        for i in range(self.batch_size):
             # Open random files with random coordinate domains until a sample contains at least one front.
-            while identifiers == 0:
-                index = random.choices(range(len(frontobject_conus_files)-1), k=1)[0]
-                with open(frontobject_conus_files[index], 'rb') as front_file:
-                    front_ds = pickle.load(front_file)
-                    lon_index = random.choices(range(289-map_size))[0]
-                    lat_index = random.choices(range(113-map_size))[0]
-                    lons = front_ds.longitude.values[lon_index:lon_index+map_size]
-                    lats = front_ds.latitude.values[lat_index:lat_index+map_size]
-                    identifiers = np.count_nonzero(front_ds.sel(longitude=lons, latitude=lats).identifier.values.flatten())
-            with open(surfacedata_conus_files[index], 'rb') as sfcdata_file:
-                sfcdata_ds = pickle.load(sfcdata_file)
-            fronts = front_ds.sel(longitude=lons, latitude=lats).to_array().T.values
-            binarized_fronts = to_categorical(fronts, num_classes=3)
-            sfcdata = sfcdata_ds.sel(longitude=lons, latitude=lats).to_array().T.values
-            front_dss[i] = binarized_fronts
-            sfcdata_dss[i] = sfcdata
-            front_file.close()
-            front_ds.close()
-            sfcdata_file.close()
-            sfcdata_ds.close()
-        yield ({input_name: sfcdata_dss},
-               {output_name: front_dss})
-
-def validation_generator(frontobject_conus_files, surfacedata_conus_files, map_size, input_name, output_name,
-                         batch_size, valid_fronts):
-    """
-    Validation generator for the Unet that generates random batches of data.
-
-    Parameters
-    ----------
-    frontobject_conus_files: list
-        List of all files containing fronts. Includes files with no fronts present in their respective domains.
-    surfacedata_conus_files: list
-        List of all files containing surface data.
-    input_name: str
-        Name of the input layer of the Unet.
-    output_name: str
-        Name of the output layer of the Unet.
-    map_size: int
-        Integer that determines the size of the window to be fed into the Unet. (e.g. Setting this to 32 would create a
-        Unet that takes input shape (batch_size, 32, 32, num_predictors)). This MUST be a multiple of 2^n.
-    batch_size: int
-        Number of images/datasets to process for each step of each epoch in the training generator. This is determined
-        by the 'valid_batch_size' argument.
-    """
-    while True:
-        front_dss = np.empty(shape=(batch_size,map_size,map_size,3))
-        sfcdata_dss = np.empty(shape=(batch_size,map_size,map_size,6))
-        for i in range(batch_size):
             identifiers = 0
-            # Open random files with random coordinate domains until a sample contains at least 'valid_fronts' fronts. 
-            while identifiers < valid_fronts:
-                index = random.choices(range(len(frontobject_conus_files)-1), k=1)[0]
-                with open(frontobject_conus_files[index], 'rb') as front_file:
+            while identifiers == 0:
+                index = random.choices(range(len(self.front_files_list)-1), k=1)[0]
+                with open(self.front_files_list[index], 'rb') as front_file:
                     front_ds = pickle.load(front_file)
-                    lon_index = random.choices(range(289-map_size))[0]
-                    lat_index = random.choices(range(113-map_size))[0]
-                    lons = front_ds.longitude.values[lon_index:lon_index+map_size]
-                    lats = front_ds.latitude.values[lat_index:lat_index+map_size]
-                    identifiers = np.count_nonzero(front_ds.sel(longitude=lons, latitude=lats).identifier.values.flatten())
-            with open(surfacedata_conus_files[index], 'rb') as sfcdata_file:
+                lon_index = random.choices(range(289-self.map_size))[0]
+                lat_index = random.choices(range(113-self.map_size))[0]
+                lons = front_ds.longitude.values[lon_index:lon_index+self.map_size]
+                lats = front_ds.latitude.values[lat_index:lat_index+self.map_size]
+                identifiers = np.count_nonzero(front_ds.sel(longitude=lons, latitude=lats).identifier.values.flatten())
+            with open(self.sfcdata_files_list[index], 'rb') as sfcdata_file:
                 sfcdata_ds = pickle.load(sfcdata_file)
             fronts = front_ds.sel(longitude=lons, latitude=lats).to_array().T.values
             binarized_fronts = to_categorical(fronts, num_classes=3)
             sfcdata = sfcdata_ds.sel(longitude=lons, latitude=lats).to_array().T.values
             front_dss[i] = binarized_fronts
             sfcdata_dss[i] = sfcdata
-            front_file.close()
-            front_ds.close()
-            sfcdata_file.close()
-            sfcdata_ds.close()
-        yield ({input_name: sfcdata_dss},
-               {output_name: front_dss})
+        return sfcdata_dss, front_dss
+
+# def training_generator(front_files_list, sfcdata_files_list, map_size, input_name, output_name, batch_size):
+#     """
+#     Training generator for the Unet that generates random batches of data.
+#
+#     Parameters
+#     ----------
+#     front_files_list: list
+#         List of all files containing fronts. Includes files with no fronts present in their respective domains.
+#     sfcdata_files_list: list
+#         List of all files containing surface data.
+#     input_name: str
+#         Name of the input layer of the Unet.
+#     output_name: str
+#         Name of the output layer of the Unet.
+#     map_size: int
+#         Integer that determines the size of the window to be fed into the Unet. (e.g. Setting this to 32 would create a
+#         Unet that takes input shape (batch_size, 32, 32, num_predictors)). This MUST be a multiple of 2^n.
+#     batch_size: int
+#         Number of images/datasets to process for each step of each epoch in the training generator. This is determined
+#         by the 'train_batch_size' argument.
+#     """
+#     while True:
+#         front_dss = np.empty(shape=(batch_size,map_size,map_size,3))
+#         sfcdata_dss = np.empty(shape=(batch_size,map_size,map_size,6))
+#         for i in range(batch_size):
+#             # Open random files with random coordinate domains until a sample contains at least one front.
+#             identifiers = 0
+#             while identifiers == 0:
+#                 index = random.choices(range(len(front_files_list)-1), k=1)[0]
+#                 with open(front_files_list[index], 'rb') as front_file:
+#                     front_ds = pickle.load(front_file)
+#                 lon_index = random.choices(range(289-map_size))[0]
+#                 lat_index = random.choices(range(113-map_size))[0]
+#                 lons = front_ds.longitude.values[lon_index:lon_index+map_size]
+#                 lats = front_ds.latitude.values[lat_index:lat_index+map_size]
+#                 identifiers = np.count_nonzero(front_ds.sel(longitude=lons, latitude=lats).identifier.values.flatten())
+#             with open(sfcdata_files_list[index], 'rb') as sfcdata_file:
+#                 sfcdata_ds = pickle.load(sfcdata_file)
+#             fronts = front_ds.sel(longitude=lons, latitude=lats).to_array().T.values
+#             binarized_fronts = to_categorical(fronts, num_classes=3)
+#             sfcdata = sfcdata_ds.sel(longitude=lons, latitude=lats).to_array().T.values
+#             front_dss[i] = binarized_fronts
+#             sfcdata_dss[i] = sfcdata
+#         yield ({input_name: sfcdata_dss},
+#                {output_name: front_dss})
+
+# def validation_generator(front_files_list, sfcdata_files_list, map_size, input_name, output_name,
+#                          batch_size, valid_fronts):
+#     """
+#     Validation generator for the Unet that generates random batches of data.
+#
+#     Parameters
+#     ----------
+#     front_files_list: list
+#         List of all files containing fronts. Includes files with no fronts present in their respective domains.
+#     sfcdata_files_list: list
+#         List of all files containing surface data.
+#     input_name: str
+#         Name of the input layer of the Unet.
+#     output_name: str
+#         Name of the output layer of the Unet.
+#     map_size: int
+#         Integer that determines the size of the window to be fed into the Unet. (e.g. Setting this to 32 would create a
+#         Unet that takes input shape (batch_size, 32, 32, num_predictors)). This MUST be a multiple of 2^n.
+#     batch_size: int
+#         Number of images/datasets to process for each step of each epoch in the training generator. This is determined
+#         by the 'valid_batch_size' argument.
+#     """
+#     while True:
+#         front_dss = np.empty(shape=(batch_size,map_size,map_size,3))
+#         sfcdata_dss = np.empty(shape=(batch_size,map_size,map_size,6))
+#         for i in range(batch_size):
+#             identifiers = 0
+#             # Open random files with random coordinate domains until a sample contains at least valid_fronts fronts. This is to
+#             # prevent the model from predicting too few fronts.
+#             while identifiers < valid_fronts:
+#                 index = random.choices(range(len(front_files_list)-1), k=1)[0]
+#                 with open(front_files_list[index], 'rb') as front_file:
+#                     front_ds = pickle.load(front_file)
+#                 lon_index = random.choices(range(289-map_size))[0]
+#                 lat_index = random.choices(range(113-map_size))[0]
+#                 lons = front_ds.longitude.values[lon_index:lon_index+map_size]
+#                 lats = front_ds.latitude.values[lat_index:lat_index+map_size]
+#                 identifiers = np.count_nonzero(front_ds.sel(longitude=lons, latitude=lats).identifier.values.flatten())
+#             with open(sfcdata_files_list[index], 'rb') as sfcdata_file:
+#                 sfcdata_ds = pickle.load(sfcdata_file)
+#             fronts = front_ds.sel(longitude=lons, latitude=lats).to_array().T.values
+#             binarized_fronts = to_categorical(fronts, num_classes=3)
+#             sfcdata = sfcdata_ds.sel(longitude=lons, latitude=lats).to_array().T.values
+#             front_dss[i] = binarized_fronts
+#             sfcdata_dss[i] = sfcdata
+#         yield ({input_name: sfcdata_dss},
+#                {output_name: front_dss})
+
 
 
 # UNet model
-def train_unet(frontobject_conus_files, surfacedata_conus_files, map_size, learning_rate, train_epochs, train_steps, 
+def train_unet(front_files_list, sfcdata_files_list, map_size, learning_rate, train_epochs, train_steps,
                train_batch_size, valid_steps, valid_batch_size, valid_freq, valid_fronts, loss, workers, job_number):
     """
     Function that trains the unet model and saves the model along with its weights.
 
     Parameters
     ----------
-    frontobject_conus_files: list
+    front_files_list: list
         List of all files containing fronts. Includes files with no fronts present in their respective domains.
-    surfacedata_conus_files: list
+    sfcdata_files_list: list
         List of all files containing surface data.
     map_size: int
         Integer that determines the size of the window to be fed into the Unet. (e.g. Setting this to 32 would create a
@@ -159,8 +195,8 @@ def train_unet(frontobject_conus_files, surfacedata_conus_files, map_size, learn
     print("\n=== MODEL TRAINING ===")
     print("Creating unet....", end='')
 
-    model = models.unet_2d((map_size, map_size, 6), filter_num=[8, 16, 32, 64, 128], n_labels=3, stack_num_down=5,
-                           stack_num_up=5, activation='LeakyReLU', output_activation='Softmax',
+    model = models.unet_2d((map_size, map_size, 6), filter_num=[4, 8, 16, 32, 64], n_labels=3, stack_num_down=5,
+                           stack_num_up=5, activation='ReLU', output_activation='Softmax',
                            batch_norm=True, pool=True, unpool=True, name='unet')
     print('done')
     print('Compiling unet....', end='')
@@ -182,16 +218,29 @@ def train_unet(frontobject_conus_files, surfacedata_conus_files, map_size, learn
     input_name = layer_names[0]
     output_name = layer_names[-1]
 
-    train_gen = training_generator(frontobject_conus_files, surfacedata_conus_files, map_size, input_name, output_name, 
-                                   train_batch_size)
-    valid_gen = validation_generator(frontobject_conus_files, surfacedata_conus_files, map_size, input_name, output_name, 
-                                     valid_batch_size, valid_fronts)
+    train_dataset = tf.data.Dataset.from_generator(DataGenerator,
+                                                   args = [front_files_list, sfcdata_files_list, map_size, train_batch_size],
+                                                   output_types=(tf.float32, tf.float32))
 
-    history = model.fit(train_gen, validation_data=valid_gen, validation_freq=valid_freq, epochs=train_epochs,
+    validation_dataset = tf.data.Dataset.from_generator(DataGenerator,
+                                                        args = [front_files_list, sfcdata_files_list, map_size, valid_batch_size],
+                                                        output_types=(tf.float32, tf.float32))
+
+    #train_gen = DataGenerator(front_files_list, sfcdata_files_list, map_size, input_name, output_name,
+    #                          train_batch_size)
+    #valid_gen = DataGenerator(front_files_list, sfcdata_files_list, map_size, input_name, output_name,
+    #                          valid_batch_size)
+
+    history = model.fit(train_dataset, validation_data=validation_dataset, validation_freq=valid_freq, epochs=train_epochs,
                         steps_per_epoch=train_steps, validation_steps=valid_steps, verbose=2, workers=workers, use_multiprocessing=True)
 
-    model.save('/work/earnestb13/ajustin/fronts/models/model_%d.h5' % job_number)
-    model.save_weights("/work/earnestb13/ajustin/fronts/models/model_%d_weights.h5" % job_number)
+    os.mkdir('/work/earnestb13/ajustin/fronts/models/model_%d' % job_number)
+    os.mkdir('/work/earnestb13/ajustin/fronts/models/model_%d/predictions' % job_number)
+    os.rename('/work/earnestb13/ajustin/fronts/TrainModel_%d_stdout.txt' % job_number,
+              '/work/earnestb13/ajustin/fronts/models/model_%d/model_%d.txt' % (job_number, job_number))
+    model.save('/work/earnestb13/ajustin/fronts/models/model_%d/model_%d.h5' % (job_number, job_number))
+    model.save_weights("/work/earnestb13/ajustin/fronts/models/model_%d/model_%d_weights.h5" % (job_number, job_number))
+    evaluate_model.evaluate_model(job_number, map_size, front_files_list, sfcdata_files_list)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
