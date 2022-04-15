aed9e691bc987e1ade83b40ff27d8deeff7daa06
Andrew Justin <76486321+andrewjustin@users.noreply.github.com>
2022-04-14 22:21:39 -0500

    update documentation

--- utils/plotting_utils.py
+++ utils/plotting_utils.py
@@ -1,4 +1,14 @@
-""" Plotting tools """
+"""
+Plotting tools
+
+Code written by: Andrew Justin (andrewjustinwx@gmail.com)
+Last updated: 4/14/2022 8:54 PM CDT
+
+Known bugs:
+- none
+
+Please report any bugs to Andrew Justin: andrewjustinwx@gmail.com
+"""
 
 import cartopy.feature as cfeature
 import cartopy.crs as ccrs
@@ -11,19 +21,24 @@ def plot_background(extent, ax=None, linewidth=0.5):
 
     Parameters
     ----------
-    extent: Numpy array containing the extent/boundaries of the plot in the format of [min lon, max lon, min lat, max lat].
-    linewidth: Thickness of coastlines and the borders of states and countries.
+    extent: iterable with 4 ints
+        - Iterable containing the extent/boundaries of the plot in the format of [min lon, max lon, min lat, max lat].
+    ax: matplotlib.axes.Axes instance or None
+        - Axis on which the background will be plotted.
+    linewidth: float
+        - Thickness of coastlines and the borders of states and countries.
 
     Returns
     -------
-    ax: New plot background.
+    ax: matplotlib.axes.Axes instance
+        - New plot background.
     """
     if ax is None:
         crs = ccrs.LambertConformal(central_longitude=250)
         ax = plt.axes(projection=crs)
     else:
-        ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth=0.25)
-        ax.add_feature(cfeature.BORDERS, linewidth=0.25)
-        ax.add_feature(cfeature.STATES, linewidth=0.25)
+        ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth=linewidth)
+        ax.add_feature(cfeature.BORDERS, linewidth=linewidth)
+        ax.add_feature(cfeature.STATES, linewidth=linewidth)
         ax.set_extent(extent, crs=ccrs.PlateCarree())
     return ax
