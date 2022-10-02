e1f0dd5fc46bd2be9b5d2d938ecd845cd04e35ce
Andrew Justin <andrewjustinwx@gmail.com>
2022-10-01 19:30:41 -0500

    updates to general debug tools
    Signed-off-by: Andrew Justin <andrewjustinwx@gmail.com>

--- debug.py
+++ debug.py
@@ -3,12 +3,13 @@ Script for running debug commands
 
 Code written by: Andrew Justin (andrewjustinwx@gmail.com)
 
-Last updated: 9/18/2022 9:10 PM CT
+Last updated: 10/1/2022 7:30 PM CT
 """
 
 import argparse
 from errors import check_arguments
-from utils.debug import model_data
+from utils.debug import model_data, era5
+import utils
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -20,8 +21,12 @@ if __name__ == '__main__':
                         help='Number of pixels to trim the images by along each dimension for stitching before taking the '
                              'maximum across overlapping pixels.')
     parser.add_argument('--find_missing_statistics', action='store_true', help='Analyze a directory of statistics to find missing data')
+    parser.add_argument('--check_era5_variables', action='store_true', help='Check variables in era5 dataset')
+    parser.add_argument('--check_for_corrupt_era5_files', action='store_true', help='Check variables in era5 dataset')
     parser.add_argument('--model_dir', type=str, help='Directory for the models.')
     parser.add_argument('--model_number', type=int, help='Model number.')
+    parser.add_argument('--era5_netcdf_indir', type=str, help='Main directory for ERA5 netcdf files')
+    parser.add_argument('--timestep', type=int, nargs=4, help='Timestep for ERA5 data (year, month, day, hour)')
     parser.add_argument('--fronts_netcdf_indir', type=str, help='Main directory for the netcdf files containing frontal objects.')
     parser.add_argument('--fronts_xml_indir', type=str, help='Main directory for the xml files containing frontal objects.')
     parser.add_argument('--variables_netcdf_indir', type=str, help='Main directory for the netcdf files containing variable data.')
@@ -37,3 +42,13 @@ if __name__ == '__main__':
         check_arguments(provided_arguments, required_arguments)
         model_data.find_missing_statistics(args.model_dir, args.model_number, args.domain, args.domain_images, args.domain_trim,
             args.variables_data_source, args.variables_netcdf_indir, args.fronts_netcdf_indir, args.fronts_xml_indir, dataset=args.dataset)
+
+    if args.check_era5_variables:
+        required_arguments = ['era5_netcdf_indir', 'timestep']
+        check_arguments(provided_arguments, required_arguments)
+        utils.debug.era5.check_era5_variables(args.era5_netcdf_indir, args.timestep)
+
+    if args.check_for_corrupt_era5_files:
+        required_arguments = ['era5_netcdf_indir']
+        check_arguments(provided_arguments, required_arguments)
+        utils.debug.era5.check_for_corrupt_era5_files(args.era5_netcdf_indir)
