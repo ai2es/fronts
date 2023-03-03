8f3aeab13f1ef29274e5954b1f1955e20993119c
Andrew Justin <andrewjustinwx@gmail.com>
2023-03-03 12:58:20 -0600

    remove unused dictionary in activation layers
    * A dictionary argument 'kwargs' in the 'call'
    attributes of the layers has been removed. These
    dictionaries were not used.
    
    Signed-off-by: Andrew Justin <andrewjustinwx@gmail.com>

--- custom_activations.py
+++ custom_activations.py
@@ -7,7 +7,7 @@ Custom activation functions:
 
 Code written by: Andrew Justin (andrewjustinwx@gmail.com)
 
-Last updated: 6/27/2022 7:13 PM CDT
+Last updated: 3/3/2023 12:51 PM CT
 """
 from tensorflow.keras.layers import Layer
 import tensorflow as tf
@@ -23,7 +23,7 @@ class Gaussian(Layer):
     def build(self, input_shape):
         """ Build the Gaussian layer """
 
-    def call(self, inputs, **kwargs):
+    def call(self, inputs):
         """ Call the Gaussian activation function """
         inputs = tf.cast(inputs, 'float32')
         square_tensor = tf.constant(2.0, shape=inputs.shape[1:])
@@ -42,7 +42,7 @@ class GCU(Layer):
     def build(self, input_shape):
         """ Build the GCU layer """
 
-    def call(self, inputs, **kwargs):
+    def call(self, inputs):
         """ Call the GCU activation function """
         inputs = tf.cast(inputs, 'float32')
         y = tf.multiply(inputs, tf.math.cos(inputs))
@@ -65,7 +65,7 @@ class SmeLU(Layer):
         """ Build the SmeLU layer """
         self.beta = self.add_weight(name='beta', dtype='float32', shape=input_shape[1:])  # Learnable parameter (see Eq. 7 in the linked paper above)
 
-    def call(self, inputs, **kwargs):
+    def call(self, inputs):
         """ Call the SmeLU activation function """
         inputs = tf.cast(inputs, 'float32')
         y = tf.where(inputs <= -self.beta, 0.0,  # Condition 1
@@ -91,7 +91,7 @@ class Snake(Layer):
         self.alpha = self.add_weight(name='alpha', dtype='float32', shape=input_shape[1:])  # Learnable parameter (see Eq. 3 in the linked paper above)
         self.square_tensor = tf.constant(2.0, shape=input_shape[1:])
 
-    def call(self, inputs, **kwargs):
+    def call(self, inputs):
         """ Call the Snake activation function """
         inputs = tf.cast(inputs, 'float32')
         y = inputs + tf.multiply(tf.divide(tf.constant(1.0, shape=inputs.shape[1:]), self.alpha), tf.math.pow(tf.math.sin(tf.multiply(self.alpha, inputs)), self.square_tensor))
