3a3334a20436657272e9b03070bc18d6d76abf90
Andrew Justin <andrewjustinwx@gmail.com>
2023-03-18 20:49:45 -0500

    adjust spline creation code
    Signed-off-by: Andrew Justin <andrewjustinwx@gmail.com>

--- create_prediction_xml.py
+++ create_prediction_xml.py
@@ -2,7 +2,7 @@
 Create an XML file containing model predictions in the form of splines.
 
 Author: Andrew Justin (andrewjustinwx@gmail.com)
-Last updated: 3/18/2023 2:35 PM CT
+Last updated: 3/18/2023 8:49 PM CT
 """
 
 
@@ -11,6 +11,7 @@ from skimage.morphology import skeletonize
 import xarray as xr
 import numpy as np
 import argparse
+from utils import settings
 
 
 FRONT_OBJ_THRESHOLDS = {'CF': {'conus': 0.35, 'full': 0.48}, 'WF': {'conus': 0.31, 'full': 0.37},
@@ -21,13 +22,13 @@ XML_FRONT_TYPE = {'CF': 'COLD_FRONT', 'CF-F': 'COLD_FRONT_FORM', 'CF-D': 'COLD_F
                   'WF': 'WARM_FRONT', 'WF-F': 'WARM_FRONT_FORM', 'WF-D': 'WARM_FRONT_DISS',
                   'SF': 'STATIONARY_FRONT', 'SF-F': 'STATIONARY_FRONT_FORM', 'SF-D': 'STATIONARY_FRONT_DISS',
                   'OF': 'OCCLUDED_FRONT', 'OF-F': 'OCCLUDED_FRONT_FORM', 'OF-D': 'OCCLUDED_FRONT_DISS',
-                  'DL': 'DRY_LINE', 'OFB': 'INSTABILITY', 'T': 'TROF', 'TT': 'TROPICAL_TROF'}
+                  'DL': 'DRY_LINE', 'OFB': 'INSTABILITY', 'T': 'TROF', 'TT': 'TROPICAL_TROF', 'F_BIN': 'BINARY_FRONT'}
 
 XML_FRONT_COLORS = {'CF': dict(red="0", green="0", blue="255"), 'CF-F': dict(red="0", green="0", blue="255"), 'CF-D': dict(red="0", green="0", blue="255"),
                   'WF': dict(red="255", green="0", blue="0"), 'WF-F': dict(red="255", green="0", blue="0"), 'WF-D': dict(red="255", green="0", blue="0"),
                   'OF': dict(red="145", green="44", blue="238"), 'OF-F': dict(red="145", green="44", blue="238"), 'OF-D': dict(red="145", green="44", blue="238"),
                   'DL': dict(red="255", green="130", blue="71"), 'OFB': dict(red="255", green="0", blue="0"), 'T': dict(red="255", green="130", blue="71"),
-                  'TT': dict(red="255", green="130", blue="71")}
+                  'TT': dict(red="255", green="130", blue="71"), 'F_BIN': dict(red="128", green="128", blue="128")}
 
 LINE_KWARGS = dict(pgenCategory="Front", lineWidth="4", sizeScale="  1.0", smoothFactor="2", closed="false", filled="false",
                    fillPattern="SOLID")
@@ -39,6 +40,7 @@ if __name__ == '__main__':
     parser.add_argument('--init_time', type=int, nargs=4, required=True, help='Date and time of the data. Pass 4 ints in the following order: year, month, day, hour')
     parser.add_argument('--forecast_hours', type=int, nargs='+', required=True, help='Forecast hour for the GDAS/GFS data.')
     parser.add_argument('--domain', type=str, required=True, help='Domain of the data.')
+    parser.add_argument('--domain_images', type=int, nargs=2, help='Lon x lat images for the predictions')
     parser.add_argument('--variable_data_source', type=str, help='Data source for variables used in the predictions.')
 
     ### Model and directory arguments ###
@@ -47,6 +49,11 @@ if __name__ == '__main__':
     parser.add_argument('--xml_dir', type=str, help='Output directory for the XML files.')
 
     args = parser.parse_args()
+
+    if args.domain_images is None:
+        domain_images = settings.DEFAULT_DOMAIN_IMAGES[args.domain]
+    else:
+        domain_images = args.domain_images
 
     for forecast_hour in args.forecast_hours:
 
@@ -58,15 +65,15 @@ if __name__ == '__main__':
         ET.SubElement(Layer, "Color", red="255", green="255", blue="0", alpha="255")
         DrawableElement = ET.SubElement(Layer, "DrawableElement")
 
-        probs_ds = xr.open_dataset('%s/model_%d/predictions/model_%d_%d-%02d-%02d-%02dz_%s_f%03d_%s_3x1_probabilities.nc' %
+        probs_ds = xr.open_dataset('%s/model_%d/predictions/model_%d_%d-%02d-%02d-%02dz_%s_f%03d_%s_%dx%d_probabilities.nc' %
             (args.model_dir, args.model_number, args.model_number, args.init_time[0], args.init_time[1], args.init_time[2],
-             args.init_time[3], args.variable_data_source, forecast_hour, args.domain)).isel(time=0)
+             args.init_time[3], args.variable_data_source, forecast_hour, args.domain, domain_images[0], domain_images[1])).isel(time=0)
+        probs_ds = probs_ds.reindex(latitude=list(reversed(probs_ds.latitude)))
+
         lons = probs_ds['longitude'].values
         lats = probs_ds['latitude'].values
 
         for key in list(probs_ds.keys()):
-
-            splines = dict()
 
             probs_ds[f'{key}_obj'] = (('longitude', 'latitude'), skeletonize(xr.where(probs_ds[key] > FRONT_OBJ_THRESHOLDS[key][args.domain], 1, 0).values))
 
@@ -80,22 +87,30 @@ if __name__ == '__main__':
             splines_made = 0
             points_in_current_spline = []
 
-            for point_no, point in enumerate(front_points):
-                front_points_dist = np.abs(np.array(front_points_copy) - np.array(point))
-                adjacent_point_indices = np.where((front_points_dist[:, 0] <= 0.25) & (front_points_dist[:, 1] <= 0.25))[0]
+            splines = dict()
+            splines['1'] = [front_points[0], ]
+            number_of_splines = 1
 
-                if len(adjacent_point_indices) > 0:
-                    if len(adjacent_point_indices) > 1 or len(points_in_current_spline) > 0:
-                        for adjacent_point in adjacent_point_indices:
-                            if front_points_copy[adjacent_point] not in points_in_current_spline:
-                                points_in_current_spline.append(front_points_copy[adjacent_point])
-                        for adjacent_point in adjacent_point_indices:
-                            front_points_copy[adjacent_point] = [-999, -999]
-                else:
-                    if len(points_in_current_spline) > 0:
-                        splines[str(splines_made + 1)] = points_in_current_spline
-                        splines_made += 1
-                    points_in_current_spline = []
+            for point in front_points[1:]:
+
+                point_adjacent_to_spline = False
+
+                for spline in splines.keys():
+                    current_spline_points = np.array(splines[spline])
+                    spline_dist_from_point = np.abs(current_spline_points - np.array(point))
+
+                    for spline_dist in spline_dist_from_point:
+                        if spline_dist[0] <= 0.25 and spline_dist[1] <= 0.25 and not any(point == point_in_spline for point_in_spline in splines[spline]) and not spline_dist[0] == spline_dist[1] == 0:
+                            point_adjacent_to_spline = True
+                            splines[spline].append(point)
+                            break
+
+                    if point_adjacent_to_spline:
+                        break
+
+                if not point_adjacent_to_spline:
+                    number_of_splines += 1
+                    splines[str(number_of_splines)] = [point, ]
 
             for spline in splines.keys():
 
@@ -114,8 +129,9 @@ if __name__ == '__main__':
                         lon_point = spline_point[0]
                     ET.SubElement(Line, "Point", Lat="%.2f" % spline_point[1], Lon="%.2f" % lon_point)
 
-        save_path_file = "%s/model_%d_splines_%d%02d%02d%02df%03d.xml" % \
-                         (args.xml_dir, args.model_number, args.init_time[0], args.init_time[1], args.init_time[2], args.init_time[3], forecast_hour)
+        save_path_file = "%s/model_%d_splines_%s_%s_%d%02d%02d%02df%03d.xml" % \
+                         (args.xml_dir, args.model_number, args.variable_data_source, args.domain, args.init_time[0],
+                          args.init_time[1], args.init_time[2], args.init_time[3], forecast_hour)
 
         ET.indent(root)
         mydata = ET.tostring(root)
